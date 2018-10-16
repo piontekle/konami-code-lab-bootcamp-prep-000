fc54329c6ec50838fb229b354b04d9eab7a38bd5
Lauren Piontek <piontekle@gmail.com>
2018-10-16 15:35:43 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -15,7 +15,9 @@ function init() {
   // your code here
   let index = 0;
   
-  
+  function konamiCodeHandler(e) {
+    const key = e.key;
+  }
   body = document.querySelector('body');
   body.addEventListener('keydown', function(konami) {
     alert("Congratulations - You've cracked the code!")
